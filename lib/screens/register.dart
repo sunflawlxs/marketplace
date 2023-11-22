6359b71c002e38e4a7286dd57e5c9202deb74f8d
sunflawlxs <sherylivana99@gmail.com>
2023-11-22 11:21:17 +0700

    tugas 9

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:marketplace/screens/login.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package: marketplace/screens/login.dart';
+
 
 // Main function to run the application
 void main() {
